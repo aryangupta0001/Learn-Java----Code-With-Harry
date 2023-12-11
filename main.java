ebbd827ee2482cba42804776c6c7c2cf39500afb
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-12-11 23:16:19 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Printing Command Line Arguments :-
+        // System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
