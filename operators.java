40beae7d61e45c34a98c3faf51b61b831850b45d
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-12-13 23:26:52 +0530

    commit

--- operators.java
+++ operators.java
@@ -1,7 +1,18 @@
 public class operators {
     public static void main(String[] args) {
         float a = 7/4.0f + 9.0f/2;
-
         System.out.println(a);
+
+        char grade = 'B';
+        // int b = grade + 8;          // char + int = int     here ASCII value of grade is added with 8
+        // System.out.println(b);
+        
+        int b = grade + (char)8;          // char + int = int     here ASCII value of grade is added with 8
+        System.out.println(b);
+
+        char c = (char)(grade + 8);          // char + int = int     here ASCII value of grade is added with 8
+        System.out.println(c);
+
+
     }
 }
